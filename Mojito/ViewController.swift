c452f792789d7123823db58f4017c1fded85bd82
punkrockgoth <sarahlacroixmusic@gmail.com>
2017-06-22 16:44:20 -0500

    Added: main button, view stats button and label. Coded main button

--- Mojito/ViewController.swift
+++ Mojito/ViewController.swift
@@ -1,6 +1,7 @@
 //
 //  ViewController.swift
 //  Mojito
+//  Prototype for Self Control app
 //
 //  Created by Sarah LaCroix on 2017-06-22.
 //  Copyright © 2017 Sarah LaCroix. All rights reserved.
@@ -10,6 +11,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    // These two functions are default and already included in the file
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +22,22 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    // Keeps count of times button has been pressed
+    var buttonCount : Int = 0
+    
+    // Default button, in centre of mainUI
+    @IBOutlet weak var btnDefaultCounter: UIButton!
+    
+    // Tells user how many times button was pressed yesterday
+    @IBOutlet weak var lblYesterdayCount: UILabel!
+    
+    
+    @IBAction func btnDefaultCounterIsClicked(_ sender: Any) {
+        buttonCount += 1
+        let buttonCountString : String = String(buttonCount)
+        
+        btnDefaultCounter.setTitle(buttonCountString, for: .normal)
+    }
 }
 
